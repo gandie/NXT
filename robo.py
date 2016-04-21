0aa1867b254e1ab565ea7548a72f021229c69bd1
gandie <lars.bergmann@perfact.de>
2016-04-21 16:35:50 +0200

    Started writing separate gamepad module

--- robo.py
+++ robo.py
@@ -250,7 +250,6 @@ class ScoutRobo(object):
         # lots of transitions found, cry for help...
         if counter > self.transition_count:
             print 'CRYING FOR HELP NOW!!!'
-            self.play_schland()
             self.transitions = []
             self.running = False
 
